db67ff5258c76a6f383428ed6745f2b3d6f94df8
Limzor <ilyailyshafedorov71@gmail.com>
2024-05-24 22:38:28 +0300

    New Changes

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import telebot
+import sqlite3
 
 token = '6816672868:AAGokrUKgeGlCjnbXVpnEXM_eiuHQxsKseg'
 
 bot = telebot.TeleBot(token)
 
-HELP = """
+db_path = "C:\Users\Limzor\Desktop\GitTele\TulaHack_Two_2.3\attractions.db" # Введите актуальный путь до db
+conn = sqlite3.connect(db_path)
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM attractions")
+
+
+ELP = """
 /help список команд
 /restart перезапустить бота
 /start начать сессию"""
